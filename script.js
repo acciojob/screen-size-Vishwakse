d4d52f59f5658eb851297b1cca2907f60e294380
Vishwakse <vishwaksen2275@gmail.com>
2026-01-05 23:22:20 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const h1=document.querySelector("h1");
+window.addEventListener('resize',()=>{
+	h1.innerHTML =`width: ${window.innerWidth} and height: ${window.innerHeight}`
+})
