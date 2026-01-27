e671a20dbe88150deb26da5b44f1a6b0543637d5
Vishwakse <vishwaksen2275@gmail.com>
2026-01-27 12:57:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-//your JS code here. If required.
-    function updateWindowSize() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    const sizeDisplay = document.querySelector('#sizeInfo h1');  
-    sizeDisplay.textContent = `Width: ${width} and Height: ${height}`;
-}
-window.addEventListener('resize', updateWindowSize);
+const head = document.querySelector("h1");
 
-updateWindowSize();
+function updateSize() {
+	head.innerHTML = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
+
+updateSize();
+window.addEventListener("resize", updateSize);
